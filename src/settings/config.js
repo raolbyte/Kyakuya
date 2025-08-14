605d8b5de7ec0c30cdf9ec973d91a8e1b1bb7832
raolsocket <raolantidonasi@gmail.com>
2025-08-15 06:01:12 +0800

    first commit

---
+++ src/settings/config.js
@@ -0,0 +1,17 @@
+export const globalConfig = {
+  owner: [""],
+  ownerName: "Raol Mukarrozi",
+  botName: "Kyakuya",
+  prefix: ".",
+  mess: {
+    ingroup: "This feature is only for groups",
+    admin: "Only group admins can use this feature",
+    owner: "You are not my owner",
+    premium: "You are not a premium user",
+    seller: "You do not have seller access",
+    wait: "Please wait, request is being processed...",
+    privateChat: "This command can only be used in private chat with bot",
+  },
+}
+
+export default globalConfig
